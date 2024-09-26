3a26dc991f855c960204d816b9ac049ca6f01405
cfoster13 <chayefoster@icloud.com>
2024-09-26 14:35:44 +0100

    Added character and item classes

--- main.py
+++ main.py
@@ -5,5 +5,19 @@ ballroom = Room("Ballroom")
 dining_hall = Room("Dining Hall")
 
 kitchen.set_description("A dank and dirty room buzzing with flies")
+ballroom.set_description("A vast room with a shiny wooden floor")
+dining_hall.set_description("A large room with ornate golden decorations")
 
-print(kitchen.get_description())
+kitchen.link_room(dining_hall, "south")
+dining_hall.link_room(kitchen, "north")
+dining_hall.link_room(ballroom, "west")
+ballroom.link_room(dining_hall, "east")
+
+current_room = kitchen
+
+while True:
+    print("\n")
+    current_room.get_details()
+    command = input("> ")
+    current_room = current_room.move(command)
+
